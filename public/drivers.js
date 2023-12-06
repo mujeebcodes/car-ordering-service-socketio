2dad245eb77e9941dca13ebb1d3b6a1d61d6fb04
mujeebcodes <mailmujeeb@icloud.com>
2023-12-06 22:38:04 +0000

    fixed the append requests in HTML

--- public/drivers.js
+++ public/drivers.js
@@ -8,10 +8,11 @@ socket.on("connect", () => {
 
 socket.on("rideRequestedData", (data) => {
   // Display the received data in the notification container
-  const requestDiv = `<div class="card" style="width: 100%"><div class="card-body"<h5 class="card-title"><strong>Ride Requested by ${data.name}</strong></h5><div> <p>Location: ${data.location}</p><p>Destination: ${data.destination}</p><p>Price: ${data.price}</p></div><div class="btn-group">
+  const requestDiv = document.createElement("div");
+  requestDiv.innerHTML = `<div class="card" style="width: 100%"><div class="card-body"<h5 class="card-title"><strong>Ride Requested by ${data.name}</strong></h5><div> <p>Location: ${data.location}</p><p>Destination: ${data.destination}</p><p>Price: ${data.price}</p></div><div class="btn-group">
   <button class="btn btn-primary btn-sm">Accept</button>
   <button class="btn btn-primary btn-sm">Reject</button>
 </div></div></div>`;
-  notifContainer.innerHTML += requestDiv;
+  notifContainer.appendChild(requestDiv);
   console.log(data);
 });
